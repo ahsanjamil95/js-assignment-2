251d50ff37cdbb9209599c71efb197b6906c2add
ahsan <ahsanjamil@live.com>
2020-06-19 22:56:46 +0500

    JS
    JS

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // JavaScript Document
+/* Chapter 21 */
+
+// 01
+
+/*var userfisName = prompt("Enter Your First Name");
+var userlasName = prompt("Enter Your Last Name");
+var userFullname=userfisName+userlasName;
+document.write(userFullname);
+*/
+
+// 02
+
+/*var mobileModel= prompt("Enter Favrouite Mobile Phone");
+document.write(mobileModel+"<br>");
+document.write("Length of user Input is: "+mobileModel.length);*/
+
+// 03
+
